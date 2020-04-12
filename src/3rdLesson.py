9293673e25a73ddf6dd82ff407e9b729b64a54eb
ammarshahin <ammar0ammar612@gmail.com>
2020-04-12 04:27:19 +0200

    Quick Save

--- src/3rdLesson.py
+++ src/3rdLesson.py
@@ -7,3 +7,7 @@ print(-2.544213)
 print(-2.544213 + 452.5465 * -534 / 4)
 print(10 % 3)
 
+my_num = 5
+print(my_num)
+print(str(my_num))  # convert the variable to string
+print("My number = " + str(my_num))
