b41db2a23b19409156855aac848d652870d38536
ammarshahin <ammar0ammar612@gmail.com>
2020-04-12 06:59:15 +0200

    Quick Save

--- src/7thLesson.py
+++ src/7thLesson.py
@@ -12,9 +12,9 @@
 # print("outside")
 
 
-def say_hi(name, age):
-    print("hello " + name + " you are " + age)
-
-
 say_hi("ammar", "25")
 say_hi("Mike", "20")
+
+
+def say_hi(name, age):
+    print("hello " + name + " you are " + age)
