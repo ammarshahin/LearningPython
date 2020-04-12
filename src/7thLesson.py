94da7f928e68d9cffbe3498b918dae602c149a6f
ammarshahin <ammar0ammar612@gmail.com>
2020-04-12 14:24:39 +0200

    Quick Save

--- src/7thLesson.py
+++ src/7thLesson.py
@@ -1,20 +1,29 @@
 # ************* Lesson 7 **************
 # ************* Functions **************
 
-# Note: Any function has an indent(Tab before any line of code inside it)
+# Note: Any function has an indent(Tab before any line of code inside the function)
 # Ex:
 # create a function
-# def say_hi():
-#     print("inside")
-#     print("inside")
-#     print("inside")
-#     print("inside")
-# print("outside")
-
+''' 
+def say_hi():
+    print("inside")
+    print("inside")
+    print("inside")
+    print("inside")
+print("outside")
+'''
+'''
+def say_hi(name, age):
+    print("hello " + name + " you are " + age)
 
 say_hi("ammar", "25")
 say_hi("Mike", "20")
+'''
 
 
-def say_hi(name, age):
-    print("hello " + name + " you are " + age)
+def cube(num):
+    return num*num*num
+
+
+print(cube(3))
+print(cube(4))
