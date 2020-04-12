eddd4b8f67a8c83d823b175570fde0c8c9e5f205
ammarshahin <ammar0ammar612@gmail.com>
2020-04-12 14:52:19 +0200

    Quick Save

--- src/8thLesson.py
+++ src/8thLesson.py
@@ -2,6 +2,16 @@
 # ************* If statement **************
 
 is_male = False
+
+if is_male:
+    print("you are male")
+elif not is_male:
+    print("you aren't a male")
+else:
+    pass # 'pass' is the keyword for continue (basically don't do anything) {it's a MUST in python!!!!}
+    
+
+'''
 is_tall = True
 
 if is_male or is_tall:
