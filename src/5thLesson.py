00c96445bf16cf6faecdd13e7a868bb63c082ac8
ammarshahin <ammar0ammar612@gmail.com>
2020-04-12 05:14:28 +0200

    Quick Save

--- src/5thLesson.py
+++ src/5thLesson.py
@@ -1,8 +1,14 @@
 # ************* Lesson 5 **************
-# ************* Lists **************
+# ************* Lists(Arrays) **************
 
+# Note: Python index the array from the start to the end && and from end to start as well but with {-} sign
+# BackEnd Indexing:
+#           -4   |  -3   |   -2   |   -1
 friends = ["Kev", "Karen", "Ahmed", "Jim"]  # array of strings
 boolArr = [True, False, False, True]  # array of boolens
 
-print(friends[1]) # print karen
-print(friends[-3]) # print karen as well
+print(friends[1])   # karen
+print(friends[-3])  # karen as well (BackEnd Indexing)
+print(friends[1:3])  # the elements from 1 to 3
+print(friends[1:])  # the elements from 1 to the end
+print(friends[:2])  # the elements from the start to the element index 2
