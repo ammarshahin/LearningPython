6b908877ba76c2caa319d51828fd06176b71629b
ammarshahin <ammar0ammar612@gmail.com>
2020-04-12 06:50:08 +0200

    Quick Save

---
+++ src/7thLesson.py
@@ -0,0 +1,19 @@
+# ************* Lesson 7 **************
+# ************* Functions **************
+
+# Note: Any function has an indent(Tab before any line of code inside it)
+# Ex: 
+# def funcName():
+#   inside       
+#   inside       
+#   inside       
+#   inside       
+# outside 
+       
+# create a function
+def say_hi():
+    print("inside")
+    print("inside")
+    print("inside")
+    print("inside")
+print("outside")
