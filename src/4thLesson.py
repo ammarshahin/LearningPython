9021083ba3d0d6b8749dc15846609fa5cca8e79c
ammarshahin <ammar0ammar612@gmail.com>
2020-04-12 04:40:18 +0200

    Quick Save

--- src/4thLesson.py
+++ src/4thLesson.py
@@ -1,7 +1,7 @@
 # ************* Lesson 4 **************
 # ************* User I/O **************
 
-# get the input from the user and store it in the variable name 
-# Note: the promote will be printed to guide the user
-name  = input()
-print("Hello " + name) 
+# get the input from the user and store it in the variable name
+# Note: the promote will be printed to guide the user[It's optional!]
+name = input("Enter your name: ")
+print("Hello " + name)
