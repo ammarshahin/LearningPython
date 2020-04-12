e8b854a22349e4050f5504e0b11507f3407d9431
ammarshahin <ammar0ammar612@gmail.com>
2020-04-12 05:58:36 +0200

    Quick Save

---
+++ src/6thLesson.py
@@ -0,0 +1,7 @@
+# ************* Lesson 6 **************
+# ************* Tuples **************
+# a tuple is a data structure that has a constant data value (Can't be changed)
+
+# create a tuple
+coordinates = (4, 5)
+print(coordinates[0], coordinates[1])
