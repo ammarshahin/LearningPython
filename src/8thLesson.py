09d755533d05ab8d55f915e8595239c6db1cc0a3
ammarshahin <ammar0ammar612@gmail.com>
2020-04-12 14:43:22 +0200

    Quick Save

---
+++ src/8thLesson.py
@@ -0,0 +1,9 @@
+# ************* Lesson 8 **************
+# ************* If statement **************
+
+isMale = True
+
+if isMale == True:
+    print("is male!!!!")
+    else:
+        pass         # 'pass' is the keyword for continue (basicly don't do anything) 
