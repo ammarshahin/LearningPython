d7059be5362d4327a547fcc2974de0488254a5ba
ammarshahin <ammar0ammar612@gmail.com>
2020-04-12 14:50:21 +0200

    Quick Save

--- src/8thLesson.py
+++ src/8thLesson.py
@@ -6,9 +6,9 @@ is_tall = True
 
 if isMale or is_tall:
     print("you are male or tall or both")
-elif isMale and is_tall:
+elif is_male and is_tall:
     print("you are male and tall")
-elif not isMale:
+elif not is_male:
     print("Not a male!!!!")
 else:
     pass # 'pass' is the keyword for continue (basically don't do anything) {it's a MUST in python!!!!}
