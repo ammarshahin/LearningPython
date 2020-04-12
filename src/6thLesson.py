87bc8058d42cf60a489641d51ff2b1839aa41cf6
ammarshahin <ammar0ammar612@gmail.com>
2020-04-12 06:05:27 +0200

    Quick Save

--- src/6thLesson.py
+++ src/6thLesson.py
@@ -1,7 +1,11 @@
 # ************* Lesson 6 **************
-# ************* Tuples **************
+# ************* Tuples(Constant Lists or Constant variables)**************
 # a tuple is a data structure that has a constant data value (Can't be changed)
+# [tuple object doesn't support item assignment]
 
 # create a tuple
 coordinates = (4, 5)
 print(coordinates[0], coordinates[1])
+
+PI = (3.1415)
+print(PI)
