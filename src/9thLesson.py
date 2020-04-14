4303c4d782e42d5b982495903dd166d5ac0ff113
ammarshahin <ammar0ammar612@gmail.com>
2020-04-14 04:45:11 +0200

    Quick Save

---
+++ src/9thLesson.py
@@ -0,0 +1,17 @@
+# ************* Lesson 8 **************
+# ************* Dictionaries **************
+
+# Dictionaries is a data structure in python that store information in key value pairs
+# In reality Dictionaries has a list of words and every word has a definition associated with it
+# In Python the word is the "Key" and the definition is the "Value"
+# Note : No duplicate keys are allowed(unique)
+
+##### ex: A program that converts Jan >> January , Feb >> February , etc #####
+monthConvertions =  # Dictionary Name
+{
+    "Jan": "January",      # Jan is the Key >> January is the value
+    "Fab": "February",
+    "Mar": "March",
+}
+
+print(monthConvertions["Jan"])
