d117b20036731171beb33405248c2ca9a68bd006
ammarshahin <ammar0ammar612@gmail.com>
2020-04-14 05:30:13 +0200

    Quick Save

--- src/10thLesson.py
+++ src/10thLesson.py
@@ -24,24 +24,29 @@ while i <= 10:
 #   loop code
 #   loop code
 # Not loop code
+
 '''
-# this loop is looping through the string
-for letter in "Giraffe Academy":
-    print(letter)
+# this loop will loop through a range of numbers(not including the last element(UpTOButNotIncluding))
+for num in range(1,10):
+    print(num)
+
 '''
+
 '''
 # this loop is looping through the Array
 num_array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 for every_element in num_array:
     print(every_element)
 '''
+
 '''
+# this loop is looping through the string
+for letter in "Giraffe Academy":
+    print(letter)
+'''
+
+
 # this loop will loop throughout the Array of strings
 friends = ["Chandler", "Rachel", "Monica", "Ross", "Phebe", "Joy"]
 for friend in friends:
     print("We Love you", friend)
-'''
-
-# this loop will loop through a range of numbers
-for num in range(1,10):
-    print(num)
