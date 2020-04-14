89acd3844615364c62209f3500f3c85cf53a809b
ammarshahin <ammar0ammar612@gmail.com>
2020-04-14 05:04:58 +0200

    Quick Save

--- src/GuessingGame.py
+++ src/GuessingGame.py
@@ -1,7 +1,9 @@
 secret_word = "Giraffe"
 guess = ""   # empty string
+no_of_guesses = 0
 
-while guess != secret_word:
+while (guess != secret_word) and (no_of_guesses < 5):
     guess = input("Enter a new guess: ")
+    no_of_guesses = + 1
 
 print("Congratulations your guess is right!!")
