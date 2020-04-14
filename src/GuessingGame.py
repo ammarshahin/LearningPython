280ac01ef1a4f735721213df12ef66ecd9b0f004
ammarshahin <ammar0ammar612@gmail.com>
2020-04-14 05:10:15 +0200

    Quick Save

--- src/GuessingGame.py
+++ src/GuessingGame.py
@@ -1,12 +1,13 @@
 secret_word = "Giraffe"
 guess = ""   # empty string
 no_of_guesses = 0
+max_no_guesses = (5)  # A tuple to store a constant value
 
-while (guess != secret_word) and (no_of_guesses < 5):
+while (guess != secret_word) and (no_of_guesses < max_no_guesses):
     guess = input("Enter a new guess: ")
     no_of_guesses += 1
 
-if no_of_guesses == 5:
+if no_of_guesses == max_no_guesses:
     print("Wrong... your guess is wrong..")
 else:
     print("Congratulations your guess is right!!")
