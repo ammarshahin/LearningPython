6af8850b1e4ba51c06e3d2c3d8197125b16eb475
ammarshahin <ammar0ammar612@gmail.com>
2020-04-14 05:19:28 +0200

    Quick Save

--- src/10thLesson.py
+++ src/10thLesson.py
@@ -32,5 +32,5 @@ for letter in "Giraffe Academy":
 
 # this loop is looping through the string by index
 num_array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-for index in num_array:
-    print(index, letter)
+for every_element in num_array:
+    print(every_element)
