e979094fda3c83cdee371f534d56b99d63887152
ammarshahin <ammar0ammar612@gmail.com>
2020-04-14 05:02:09 +0200

    Quick Save

--- src/10thLesson.py
+++ src/10thLesson.py
@@ -1,6 +1,14 @@
 # ************* Lesson 10 **************
 # ************* While Loop *************
 
+# ex:
+# while <Condition>:
+#   loop code
+#   loop code
+#   loop code
+#   loop code
+# Not loop code
+
 i = 1
 while i <= 10:
     print(i)
---
+++ src/GuessingGame.py
@@ -0,0 +1,7 @@
+secret_word = "Giraffe"
+guess = ""   # empty string
+
+while guess != secret_word:
+    guess = input("Enter a new guess: ")
+
+print("Congratulations your guess is right!!")
