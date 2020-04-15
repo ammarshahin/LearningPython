4f7375a8e6df9ca5f4540af3f6758e8c709bcc90
ammarshahin <ammar0ammar612@gmail.com>
2020-04-15 07:34:14 +0200

    Quick Save

---
+++ src/11thLesson.py
@@ -0,0 +1,12 @@
+# ************* Lesson 11 **************
+# ************* Exponent Function ******
+
+# Note: 2**3 == 2^3
+
+
+def raise_to_power(num, pow):
+    for index in range(1, pow+1):
+        num *= num
+
+
+print(raise_to_power(2, 2))
