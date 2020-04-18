d461defd08a71fbe8df5141b029a723ec1f61c7f
ammarshahin <ammar0ammar612@gmail.com>
2020-04-18 13:30:49 +0200

    Quick Save

--- src/13thLesson.py
+++ src/13thLesson.py
@@ -1,8 +1,12 @@
 # ************* Lesson 13  **************
 # ************* Try Except **************
-
+# Note: is used run a test on our program to test it against any run time error 
+# we can specify the type of error every except handle
 try:
-    num = int(input("Enter a number: "))
+    num1 = 10/0 
+    num2 = int(input("Enter a number: "))
     print("the number is", num)
-except:
+except ZeroDivisionError as err: 
+    print(err)
+except ValueError:
     print("Not a number!!")
