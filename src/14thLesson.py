24c30e5d64fcf3e3108498139f7ab41f518d26cf
ammarshahin <ammar0ammar612@gmail.com>
2020-04-18 13:56:00 +0200

    Quick Save

--- src/14thLesson.py
+++ src/14thLesson.py
@@ -7,7 +7,7 @@
 employee_file = open("src/AttachmentTo14thLesson.txt", "r")
 
 if employee_file.readable():            # check if the file is readable or not 
-    print(employee_file.read())         # read the entire file 
+    #print(employee_file.read())         # read the entire file 
     print(employee_file.readline())     # read the first line
     print(employee_file.readline())     # read the second line
     print(employee_file.readlines())    # read the all the lines and store them in a list
