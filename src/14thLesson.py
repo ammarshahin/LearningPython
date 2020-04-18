cd6a28b89553efd9622e2c5b4e073a05ac89e95e
ammarshahin <ammar0ammar612@gmail.com>
2020-04-18 13:46:46 +0200

    Quick Save

--- src/14thLesson.py
+++ src/14thLesson.py
@@ -3,7 +3,8 @@
 # Attach : AttachmentTo14thLesson.txt
 
 # open a the file and (r[read], w[writ], a[append], r+[read an write])
-employee_file = open("src/AttachmentTo14thLesson.txt", "r")
+# employee_file is an object the stores the file inside it
+employee_file = open("src/AttachmentTo14thLesson.txt", "r+")
 
 if employee_file.readable():
     print(employee_file.read())
