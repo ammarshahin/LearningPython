1905ac03e9ea4c24bb6df0d457dc92fba99b4c1a
ammarshahin <ammar0ammar612@gmail.com>
2020-04-18 13:59:45 +0200

    Quick Save

--- src/14thLesson.py
+++ src/14thLesson.py
@@ -9,9 +9,12 @@ if employee_file.readable():            # check if the file is readable or not
     #print(employee_file.read())         # read the entire file 
     #print(employee_file.readline())     # read the first line
     #print(employee_file.readline())     # read the second line
-    print(employee_file.readlines())    # read the all the lines and store them in a list
+    employee_list = employee_file.readlines()    # read the all the lines and store them in a list
 else:
     print("Not a readable file !!!")
+
+for line in employee_list:
+    print(line)
 
 employee_file.close()  # close the opened file
 
