ff3bb15e4e96734a41dc5c8d6397ec32f543e745
ammarshahin <ammar0ammar612@gmail.com>
2020-04-18 14:19:36 +0200

    Quick Save

--- src/16thLesson.py
+++ src/16thLesson.py
@@ -1,6 +1,10 @@
 # ************* Lesson 16  **************
 # *********** Modules and Pip **************
 
-import BasicCalculator
+import BasicCalculator  # Import the Entire BasicCalculator.py file into this file
 
+# Access a specific function or data in the BasicCalculator.py file
 print(BasicCalculator.factorial(10))
+
+# Note: there is a huge list of python modules on internet
+# https://docs.python.org/3/py-modindex.html
