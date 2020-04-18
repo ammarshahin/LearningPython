95ebf3cc8361c87f2d665c0b812398429beff2f2
ammarshahin <ammar0ammar612@gmail.com>
2020-04-18 13:55:43 +0200

    Quick Save

--- src/14thLesson.py
+++ src/14thLesson.py
@@ -1,14 +1,16 @@
 # ************* Lesson 14  **************
 # *********** Reading files **************
-# Attach : AttachmentTo14thLesson.txt
+# Attach : src/AttachmentTo14thLesson.txt
 
 # open a the file and (r[read], w[writ], a[append], r+[read an write])
 # employee_file is an object the stores the file inside it
-employee_file = open("src/AttachmentTo14thLesson.txt", "r+")
+employee_file = open("src/AttachmentTo14thLesson.txt", "r")
 
-print(employee_file.read())
-if employee_file.readable():
-    print("DFD")
+if employee_file.readable():            # check if the file is readable or not 
+    print(employee_file.read())         # read the entire file 
+    print(employee_file.readline())     # read the first line
+    print(employee_file.readline())     # read the second line
+    print(employee_file.readlines())    # read the all the lines and store them in a list
 else:
     print("Not a readable file !!!")
 
