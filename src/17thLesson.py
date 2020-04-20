7ba969ea389203554f4fcc3d62bdb6b789f0f68e
ammarshahin <ammar0ammar612@gmail.com>
2020-04-20 15:48:40 +0200

    Quick Save

---
+++ src/17thLesson.py
@@ -0,0 +1,10 @@
+# ************* Lesson 17  ****************
+# *********** Classes & Objects **************
+
+from Student import Student
+
+s1 = Student("Jim", "Eng", 3.1)
+
+print(s1.name)
+print(s1.major)
+print(s1.gpa)
---
+++ src/student.py
@@ -0,0 +1,5 @@
+class Student:  # define a class
+    def __init__(self, name, major, gpa):  # initialization Function
+        self.name = name
+        self.major = major
+        self.gpa = gpa
