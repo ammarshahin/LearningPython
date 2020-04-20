7dc0b00ff963d915a3440eedc901c783242bad27
ammarshahin <ammar0ammar612@gmail.com>
2020-04-20 15:54:48 +0200

    Quick Save

--- src/17thLesson.py
+++ src/17thLesson.py
@@ -1,10 +1,12 @@
 # ************* Lesson 17  ****************
 # *********** Classes & Objects **************
 
-from Student import Student
+# from Student import Student # from the module improt the class ( "*" to import all the file)
+# from the module improt the class ( "*" to import all the file)
+from Student import *
 
 s1 = Student("Jim", "Eng", 3.1)
 
 print(s1)
-#print(s1.major)
-#print(s1.gpa)
+# print(s1.major)
+# print(s1.gpa)
