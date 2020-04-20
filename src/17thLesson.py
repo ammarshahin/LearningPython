018c47b03d341431b0b948ebb4b968507436a5b8
ammarshahin <ammar0ammar612@gmail.com>
2020-04-20 16:11:13 +0200

    Quick Save

--- src/17thLesson.py
+++ src/17thLesson.py
@@ -1,8 +1,8 @@
 # ************* Lesson 17  ****************
 # *********** Classes & Objects **************
 
-# from Student import Student # from the module improt the class ( "*" to import all the file)
-#from Student import *         # from the module improt ( "*" to import all the file)
+# from Student import Student   # from the module improt the class ( "*" to import all the file)
+# from Student import *         # from the module improt ( "*" to import all the file)
 
 class Student:  # define a class
     def __init__(self, name, major, gpa):  # initialization  Function (Constructor)
@@ -10,7 +10,7 @@ class Student:  # define a class
         self.major = major
         self.gpa = gpa
 
-s1 = Student("Jim", "Eng", 3.1)
+s1 = Student("Jim", "Eng", 3.1) # creating an object
 
 print(s1.name)
 print(s1.major)
