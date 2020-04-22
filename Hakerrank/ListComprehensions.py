fa18f91338088a104c38e326607d735e86870099
ammarshahin <ammar0ammar612@gmail.com>
2020-04-22 04:49:22 +0200

    Quick Save

--- Hakerrank/ListComprehensions.py
+++ Hakerrank/ListComprehensions.py
@@ -1,11 +1,10 @@
-if __name__ == '__main__':
-    x = int(input())
-    y = int(input())
-    z = int(input())
-    n = int(input())
+x = int(input())
+y = int(input())
+z = int(input())
+n = int(input())
 
 list_of_coordinates = []
-sigle_coordinate = []
+single_coordinate = []
 i = 0
 j = 0
 k = 0
@@ -14,5 +13,5 @@ while i < x:
     while j < y:
         while k < Z:
             if i+j+k < n:
-                sigle_coordinate = [i,j,k]
+                sigle_coordinate = [i, j, k]
                 print(sigle_coordinate)
