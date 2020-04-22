4e8a68edbab7c676099c6ec7e9456247440d28b9
ammarshahin <ammar0ammar612@gmail.com>
2020-04-22 05:10:15 +0200

    Quick Save

--- Hakerrank/ListComprehensions.py
+++ Hakerrank/ListComprehensions.py
@@ -4,14 +4,14 @@ z = int(input())
 n = int(input())
 
 list_of_coordinates = []
-i = 0
 
+i = 0
 while i <= x:
     j = 0
     while j <= y:
         k = 0
         while k <= z:
-            if i+j+k <= n:
+            if i+j+k != n:
                 single_coordinate = [i, j, k]
                 list_of_coordinates.append(single_coordinate)
             k += 1
