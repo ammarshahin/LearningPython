82033474b4ad682548725347d546b25c191c421f
ammarshahin <ammar0ammar612@gmail.com>
2020-04-22 05:12:38 +0200

    Quick Save

--- Hakerrank/FindtheRunner-UpScore.py
+++ Hakerrank/FindtheRunner-UpScore.py
@@ -1,3 +1,4 @@
-n = int(input())
-arr = map(int, input().split())
-print(arr)
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    print(sorted(list(set(arr)))[-2])
